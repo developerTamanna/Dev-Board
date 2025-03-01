9cb5adc382d9c3ac5520441e76a26b573292c9c2
developerTamanna <astamanna57@gmail.com>
2025-03-01 17:46:51 +0600

    rendom-colortheme js

--- script/script.js
+++ script/script.js
@@ -18,9 +18,16 @@ const year = currentDate.getFullYear();
 document.getElementById('current-date').innerText= `${dayName}, ${monthName} ${date}, ${year}
 `;
 
+
 // theme part change dynamically
 
 
-
+const changeButton = document.getElementById('change-bg');
+changeButton.addEventListener('click',function(){
+    const body = document.body;
+    const colors = ['#f0f8ff', '#ffebcd', '#8a2be2', '#a52a2a', '#5f9ea0', '#d2691e', '#ff7f50'];
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    body.style.backgroundColor = randomColor
+})
 
 
